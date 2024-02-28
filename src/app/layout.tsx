4f613afad81a2e3db551e929eac5ecc61a511692
Narcizo Palioto <narcizo.gabriel2@gmail.com>
2024-02-28 17:51:16 -0300

    meta tag added for https requests

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
+            <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
             <body>{children}</body>
         </html>
     );
